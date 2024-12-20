c702f4da479e4404f37fe5705f52d74ef3b56569
Lora <andreslora010@gmail.com>
2024-12-20 16:38:19 -0500

    Diseño inicial login v1, se guardan cambios por cambio de pc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./views/login/login.component";
+import { ComponentInlineComponent } from './views/component-inline/component-inline.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, ComponentInlineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'EdenSkyFront';
+  title = 'Bienvenido a EdenSkyFront';
 }
