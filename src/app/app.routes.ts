b7e203143d875c01ab0a7b606cc841da4d15016b
Lora <andreslora010@gmail.com>
2024-12-22 00:32:52 -0500

    Se realizar la configuracion de rutas y el llamado al servicio de test del backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'Dashboardbarber',title: 'Dashboard barberia',component: DashboardComponent},
+    {path:'',title:'login',component: LoginComponent}
+];
+
---
+++ src/app/core/Api/api.service.ts
@@ -0,0 +1,4 @@
+export var API = {
+  /** API para los servicios de TEST */
+  url : 'http://localhost:5000/api/health-checks/'
+}
